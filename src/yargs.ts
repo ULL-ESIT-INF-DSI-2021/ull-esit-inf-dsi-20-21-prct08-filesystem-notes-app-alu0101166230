3279548754008652cbab7d3a1d2b26adea103454
Leonardo Dorta <alu0101166230@ull.edu.es>
2021-04-24 21:04:14 +0100

    Adding functionality implemented

---
+++ src/yargs.ts
@@ -0,0 +1,53 @@
+const {help} = require('yargs');
+const yargs = require('yargs');
+const argv = require('yargs/yargs')(process.argv.slice(2));
+import {createNote, removeNote} from './index';
+
+interface Arguments {
+  title: string,
+  body: string
+}
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    user: {
+      describe: 'Note Owner',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note description',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: (argv:Arguments) => createNote(argv.title, argv.body),
+});
+help();
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove an existend note',
+  builder: {
+    user: {
+      describe: 'Note Owner',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: (argv:Arguments) => removeNote(argv.title),
+});
+
+yargs.parse();
