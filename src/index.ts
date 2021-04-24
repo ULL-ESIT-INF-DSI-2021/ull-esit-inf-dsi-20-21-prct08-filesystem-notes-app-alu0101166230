145a11a26853d764a00611ef4458113ab9982f44
Leonardo Dorta <alu0101166230@ull.edu.es>
2021-04-24 12:27:05 +0100

    Adding yarg

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import * as chalk from 'chalk';
+// const chalk = require('chalk');
 
-chalk.blue('hello');
+// const miles:number = 18;
+
+// const color:string = 'blue';
+
+// const calculateFeet = (miles:number) => miles * 5280;
+
+// console.log(chalk`
+// 	There are {bold 5280 feet} in a mile.
+// 	In {bold ${miles} miles}, there are {green.bold ${calculateFeet(miles)} feet}.
+// `);
+
+// console.log(chalk `{${color} asdasd}`);
